70806b678c5963fc2404c0ae7bf672a74f99a020
srrfrhmnn <srrfrhmn@gmail.com>
2021-12-28 17:10:07 -0500

    Implemented button and evaluation functionality

--- script.js
+++ script.js
@@ -22,4 +22,35 @@ function operate(a,b,operation){
     return operation(a,b);
 }
 
-console.log(operate(2,3,add));
+
+//add event listener to all divs with class "button"
+document.querySelectorAll(".button").forEach(function(element){
+    element.addEventListener("click", function(event){
+        //get the value of the button that was clicked
+        let value = event.target.innerText;
+        //get the display element
+        let display = document.getElementById("display");
+        //get the current value of the display
+        let currentValue = display.innerText;
+        //check if the value is a number
+        if(!isNaN(value)){
+            //add the number to the end of the current value
+            display.innerText = currentValue + value;
+        } else {
+            //if the value is not a number, check what kind of button it is
+            if(value == "clear"){
+                //if it is the clear button, set the display text to an empty string
+                display.innerText = "";
+            } else if(value == "="){
+                //if it is the equals button, calculate the value of the display text
+                display.innerText = eval(display.innerText);
+            } else if(value == "delete"){
+                //if it is the delete button, remove the last character from the display text
+                display.innerText = currentValue.slice(0, -1);
+            } else {
+                //if it is any other button, add the value to the end of the display text
+                display.innerText = currentValue + value;
+            }
+        }
+    });
+});
